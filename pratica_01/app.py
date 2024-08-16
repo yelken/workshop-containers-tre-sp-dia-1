aa406cb9afec0313582cba89bcfa06681ce7e62f
Yelken Gonzales <yelkezin@gmail.com>
2024-08-16 17:11:09 -0300

    Update app.py

--- pratica_01/app.py
+++ pratica_01/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Olá, TRE-SP 16/08!"
+    return "Olá, TRE-SP 16/58!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
